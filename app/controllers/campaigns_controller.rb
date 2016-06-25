a6ec221afccdd9525f1a02ed1f379222fde3fd3d
Sanghyun Park <sh@baxang.com>
2016-06-25 14:35:50 +1000

    Add Campaign model and a form on home page.

---
+++ app/controllers/campaigns_controller.rb
@@ -0,0 +1,35 @@
+class CampaignsController < ApplicationController
+  before_action :set_campaign, only: [:show]
+
+  # GET /campaigns/1
+  # GET /campaigns/1.json
+  def show
+  end
+
+  # POST /campaigns
+  # POST /campaigns.json
+  def create
+    @campaign = Campaign.new(campaign_params)
+
+    respond_to do |format|
+      if @campaign.save
+        format.html { redirect_to @campaign, notice: 'Campaign was successfully created.' }
+        format.json { render :show, status: :created, location: @campaign }
+      else
+        format.html { render :new }
+        format.json { render json: @campaign.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_campaign
+      @campaign = Campaign.find(params[:id])
+    end
+
+    # Never trust parameters from the scary internet, only allow the white list through.
+    def campaign_params
+      params.require(:campaign).permit(:title)
+    end
+end
